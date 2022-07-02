e7aca775413e26585d624bd283dd2f916c612bf6
Shashwattejaswi <shashwatteju06@gmail.com>
2022-07-03 02:21:53 +0530

    set function for counting and levelstoryReturn

--- main.js
+++ main.js
@@ -1,28 +1,67 @@
 function start(){
     document.getElementsByClassName('popupContainer')[0].remove();
+   
 }
 
-const story={
+const storys={
    mangal:{
-            1:'level1',
-            2:'level2',
-            3:'level3',
-            4:'level4',
-            5:'level5'
+            level1:'level1',
+            level2:'level222',
+            level3:'level3',
+            level4:'level4',
+            level5:'level5'
         },
    krutiDev:{
-            1:'level1',
-            2:'level2',
-            3:'level3',
-            4:'level4',
-            5:'level5'
+            level1:'level1',
+            level2:'level2',
+            level3:'level3',
+            level4:'level4',
+            level5:'level5'
         },
    english:{
-            1:'level1',
-            2:'level2',
-            3:'level3',
-            4:'level4',
-            5:'level5'
+            level1:'level1',
+            level2:'level2',
+            level3:'level3',
+            level4:'level4',
+            level5:'level5'
         },
-    
+   setLanguageLevel:function(languageName,levels){
+      return this[languageName][levels];
+   }
+}
+
+function countdown(time=1){
+    let secondOnes=0;
+    let secondTens=0;
+    let minuteOnes=time;
+    let minuteTens=0;
+    let timeshow=document.getElementById('timers');
+    var  setvalue=setInterval(function()
+    {
+        if(timeshow.value==0+''+0+':'+0+''+0)
+        {
+            frontPopup(true);
+            clearInterval(setvalue);  
+        }
+
+        else{  
+             if(secondOnes==0)
+                    secondOnes=9;
+                else{secondOnes--}
+
+            if(secondOnes==9)secondTens--;
+
+            if(secondTens==-1){
+                minuteOnes--;
+                secondTens=5;
+            }
+
+            if(minuteOnes==-1){
+                minuteTens--;
+                minuteOnes=9;
+            }
+
+            timeshow.value=minuteTens+''+minuteOnes+':'+secondTens+''+secondOnes;
+        }
+    },1000);
 }

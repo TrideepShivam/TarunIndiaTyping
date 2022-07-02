86c28d1563974063f9aacebaf404c84dae7f2397
shashwattejaswi <shashwatteju06@gmail.com>
2022-07-02 20:08:12 +0530

    created object for subjects and levels.

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
 function start(){
     document.getElementsByClassName('popupContainer')[0].remove();
 }
+
+const story={
+   mangal:{
+            1:'level1',
+            2:'level2',
+            3:'level3',
+            4:'level4',
+            5:'level5'
+        },
+   krutiDev:{
+            1:'level1',
+            2:'level2',
+            3:'level3',
+            4:'level4',
+            5:'level5'
+        },
+   english:{
+            1:'level1',
+            2:'level2',
+            3:'level3',
+            4:'level4',
+            5:'level5'
+        },
+    
+}

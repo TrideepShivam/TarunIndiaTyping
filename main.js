7ec84588b539bdf4bac4ff7f38cc4c647faef279
trideepshivam <trideepshivam@gmail.com>
2022-07-06 01:46:02 +0530

    created global object to handle all of the typed content and highlight it and many more

--- main.js
+++ main.js
@@ -1,3 +1,55 @@
+let CurrentState={
+    readableCurrentIndex:20,
+    readableWord:"",
+    readableNextIndex:0,
+    readableNextWord:"",
+    writtenCurrentIndex:-1,
+    writtenNextIndex:0,
+    writtenWord:function(){
+        let content = document.getElementById('write').value;
+        word = content.substring(this.writtenCurrentIndex+1,this.writtenNextIndex);
+        this.writtenCurrentIndex=this.writtenNextIndex;
+        console.log(this.writtenCurrentIndex+" "+word);
+        return word;
+    },
+    findWords:function(){
+        let content = document.getElementById('read').innerHTML;
+        let counter = this.readableCurrentIndex+1;
+        let character=content[counter];        
+        let word="";
+        while(character!=" "){
+            word=word+character;
+            counter++;
+            character=content[counter];
+        }
+        this.readableCurrentIndex=counter;
+        word=word.substring(0,word.length-7);
+        this.readableWord=word;
+
+        counter++;
+        character=content[counter];
+        word="";
+        while(character!=" "){
+            word=word+character;
+            counter++;
+            character=content[counter];
+        }
+        this.readableNextIndex=counter;
+        this.readableNextWord=word;
+    },
+    isSameWord:function(){
+        this.findWords()
+        let readable = this.readableWord;
+        let written = this.writtenWord();
+        console.log(readable);
+        console.log(written);
+        if(readable==written)
+            return true;
+        else
+            return false;
+    }
+}
+
 const storys={
     Mangal:{
              level1:'level1 is good ',
@@ -14,7 +66,7 @@ const storys={
              level5:'level5'
          },
     English:{
-             level1:`A long time ago, during a devastating spell of dry weather, there was a thirsty crow who was desperate for a drink of water. The poor crow flew and flew in search of a means to quench his thirst. From one place to another he went, until, at long last, when he could fly no further, he came upon a large pitcher of water at the base of a tree. Overjoyed, the thirsty crow thrust its jet-black beak inside to drink his fill. But, alas, the pitcher had a narrow neck! Try as he might, the despairing crow couldn’t get his head far enough inside. To his dismay, he realized the water was out of reach. The thirsty crow cried out and flapped his wings in anguish. He attempted to knock the pitcher over. But to no avail. It was too heavy for his weary, dehydrated body to budge. The crow was on the verge of exhaustion and ready to fly elsewhere. But then an idea came to him. Around the base of the pitcher sat some small round pebbles. Picking them up, one by one, the thirsty crow dropped them into the pitcher. Again and again he placed these stones inside. And with every extra pebble, the water level began to rise. His idea worked. Eventually, after much toil, the water rose so high inside the pitcher that the clever crow was able to drink his fill and sate his thirst for good.`,
+             level1:`<span id='highlight'>A</span> long time ago, during a devastating spell of dry weather, there was a thirsty crow who was desperate for a drink of water. The poor crow flew and flew in search of a means to quench his thirst. From one place to another he went, until, at long last, when he could fly no further, he came upon a large pitcher of water at the base of a tree. Overjoyed, the thirsty crow thrust its jet-black beak inside to drink his fill. But, alas, the pitcher had a narrow neck! Try as he might, the despairing crow couldn’t get his head far enough inside. To his dismay, he realized the water was out of reach. The thirsty crow cried out and flapped his wings in anguish. He attempted to knock the pitcher over. But to no avail. It was too heavy for his weary, dehydrated body to budge. The crow was on the verge of exhaustion and ready to fly elsewhere. But then an idea came to him. Around the base of the pitcher sat some small round pebbles. Picking them up, one by one, the thirsty crow dropped them into the pitcher. Again and again he placed these stones inside. And with every extra pebble, the water level began to rise. His idea worked. Eventually, after much toil, the water rose so high inside the pitcher that the clever crow was able to drink his fill and sate his thirst for good.`,
              level2:'level2',
              level3:'level3',
              level4:'level4',
@@ -94,17 +146,34 @@ function countdown(time,secondTens,secondOnes){
 }
 
 document.addEventListener('keyup',function(event){
-var x = event.key;
-if(x=='Pause')
-{
-clearInterval(setvalue);
-let timeshowValue=document.getElementById('timers').value;
-min=timeshowValue.slice(0,2);
-secTens=timeshowValue.slice(3,4);
-secOnes=timeshowValue.slice(4,5);
-}
-if(x=='ArrowRight'){
-    
-    countdown(min,secTens,secOnes);
-}
+    var x = event.key;
+    if(x=='Pause'){
+        clearInterval(setvalue);
+        let timeshowValue=document.getElementById('timers').value;
+        min=timeshowValue.slice(0,2);
+        secTens=timeshowValue.slice(3,4);
+        secOnes=timeshowValue.slice(4,5);
+    }
+    if(x=='ArrowRight'){        
+        countdown(min,secTens,secOnes);
+    }
 });
+
+function highlightNext(){
+    let highlightedArea = document.getElementById('highlight').innerHTML;
+    let content = document.getElementById('read');
+    let nonHighlightedArea = content.innerHTML.substring(CurrentState.readableNextIndex);
+    content.innerHTML='<span id="highlight">'+highlightedArea+' '+CurrentState.readableNextWord+'</span>'+nonHighlightedArea;
+    CurrentState.readableCurrentIndex-=7;
+}
+
+function typing(currentEle,e){
+    let content= currentEle.value;
+    let newKey= e.key;
+    if(newKey==' ' && content!=""){
+        CurrentState.writtenNextIndex=content.length-1;
+        console.log(CurrentState.isSameWord());
+        highlightNext();
+        console.log(CurrentState.readableCurrentIndex);
+    }
+}
